747e04ceab6fc624b0457c2f2b61e579f1944675
Mitch Henline <rmhenline@gmail.com>
2022-09-30 10:16:56 -0600

    sdlc finished

--- sdlc.js
+++ sdlc.js
@@ -17,7 +17,20 @@
 
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of
+     each variable to a definition of the step.
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step where it is determined what resources, personnel and tools are needed to finish the software."
+const design = "This is done before the code is written, but where the ideas come together and the pieces are put together to determine how it will operate"
+const implementation = "The fourth step is where the design is put into code and the software can begin to come alive."
+const testingAndIntegration = "The fifth step. The software now exists, but there are likely bugs to be worked out. Each function must be tested to ensure it works."
+const maintain = "Problems amy arise. Improvements need to be made. Software may need to be updated. Maintain is making sure it keeps working."
+
+//console.log(planning)
+//console.log(analysisOfRequirements)
+//console.log(design)
+//console.log(implementation)
+//console.log(testingAndIntegration)
+//console.log(maintain)
